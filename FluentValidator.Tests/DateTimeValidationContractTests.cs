22561b5bca823f3da81bfdf213f8f12573c5f181
= <fabiogemignani@outlook.com>
2017-09-26 16:22:36 -0300

    Added datetime validations

---
+++ FluentValidator.Tests/DateTimeValidationContractTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentValidator.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FluentValidator.Tests
+{
+    [TestClass]
+    class DateTimeValidationContractTests
+    {
+        [TestMethod]
+        [TestCategory("DateTimeValidation")]
+        public void IsGreaterThan()
+        {
+            var date1 = DateTime.Now;
+            var date2 = date1.AddMinutes(1);
+
+            var wrong = new ValidationContract()
+                .Requires()
+                .IsGreaterThan(date2, date1, "DateTime", "Date 2 is greater than Date 1");
+
+            Assert.AreEqual(false, wrong.IsValid);
+            Assert.AreEqual(1, wrong.Notifications.Count);          
+        }
+
+        [TestMethod]
+        [TestCategory("DateTimeValidation")]
+        public void IsGreaterOrEqualsThan()
+        {
+            var date1 = DateTime.Now;
+            var date2 = date1;
+
+            var wrong = new ValidationContract()
+                .Requires()
+                .IsGreaterOrEqualsThan(date2, date1, "DateTime", "Date 2 is greater or equals than Date 1");
+
+            Assert.AreEqual(false, wrong.IsValid);
+            Assert.AreEqual(1, wrong.Notifications.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("DateTimeValidation")]
+        public void IsLowerThan()
+        {
+            var date1 = DateTime.Now;
+            var date2 = date1.AddMinutes(-1);
+
+            var wrong = new ValidationContract()
+                .Requires()
+                .IsLowerThan(date1, date2, "DateTime", "Date 1 is lower than Date 2");
+
+            Assert.AreEqual(false, wrong.IsValid);
+            Assert.AreEqual(1, wrong.Notifications.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("DateTimeValidation")]
+        public void IsLowerOrEqualsThan()
+        {
+            var date1 = DateTime.Now;
+            var date2 = date1;
+
+            var wrong = new ValidationContract()
+                .Requires()
+                .IsLowerOrEqualsThan(date1, date2, "DateTime", "Date 1 is lower than Date 2");
+
+            Assert.AreEqual(false, wrong.IsValid);
+            Assert.AreEqual(1, wrong.Notifications.Count);
+        }
+    }
+}
