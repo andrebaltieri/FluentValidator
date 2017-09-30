a40c65c41550b1f8972bb586710329f7613edb88
Milton Câmara <miltomcamara@gmail.com>
2017-09-30 13:35:16 -0300

    Added FunctionValidationContract

---
+++ FluentValidator/Validation/FunctionValidationContract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentValidator.Validation
+{
+    public partial class ValidationContract
+    {
+        public ValidationContract Should<T>(Func<T, bool> obj, T value, string message)
+        {
+            var objResult = obj.Invoke(value);
+            if (objResult)
+                AddNotification(((System.Reflection.RuntimeMethodInfo)obj.Method).Name, message);
+
+            return this;
+        }
+    }
+}
